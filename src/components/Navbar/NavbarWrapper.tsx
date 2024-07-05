de2e1d4d2dbd2b2c785c5069bbf7b3c7298c5d19
Tommy <itsTomLie@gmail.com>
2024-07-05 21:37:33 +0800

    Fixed multiple bugs for deploying

--- src/components/Navbar/NavbarWrapper.tsx
+++ src/components/Navbar/NavbarWrapper.tsx
@@ -1,11 +1,13 @@
 import { createClient } from "@/utils/supabase/server";
 import Navbar from "./Navbar";
 
-export default async function NavbarWrapper() {
+const NavbarWrapper = async () => {
   const supabase = createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
   return <Navbar user={user} />;
-}
+};
+
+export default NavbarWrapper;
